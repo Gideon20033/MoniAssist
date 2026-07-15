2d4a05c0317e9e06063b137f229ae690f148a581
Gideon20033 <gideonjeremiah2022@gmail.com>
2026-07-16 00:59:24 +0100

    Create vip.js with VIP plans data
    Added an array of VIP plans with investment details.

---
+++ vip.js
@@ -0,0 +1,58 @@
+const vipPlans = [
+  {
+    vip: "VIP 1",
+    investment: 3000,
+    dailyIncome: 750,
+    totalIncome: 67500,
+    duration: 90
+  },
+  {
+    vip: "VIP 2",
+    investment: 7000,
+    dailyIncome: 1750,
+    totalIncome: 157500,
+    duration: 90
+  },
+  {
+    vip: "VIP 3",
+    investment: 15000,
+    dailyIncome: 3850,
+    totalIncome: 346500,
+    duration: 90
+  },
+  {
+    vip: "VIP 4",
+    investment: 35000,
+    dailyIncome: 8900,
+    totalIncome: 801000,
+    duration: 90
+  },
+  {
+    vip: "VIP 5",
+    investment: 70000,
+    dailyIncome: 18550,
+    totalIncome: 1669500,
+    duration: 90
+  },
+  {
+    vip: "VIP 6",
+    investment: 150000,
+    dailyIncome: 40000,
+    totalIncome: 3600000,
+    duration: 90
+  },
+  {
+    vip: "VIP 7",
+    investment: 360000,
+    dailyIncome: 100000,
+    totalIncome: 9000000,
+    duration: 90
+  },
+  {
+    vip: "VIP 8",
+    investment: 550000,
+    dailyIncome: 150000,
+    totalIncome: 13500000,
+    duration: 90
+  }
+];
